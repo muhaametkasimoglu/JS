52ff0337f612aff95d8168c8eb247f9b51cb8094
muhaametkasimoglu <muhammetkasimoglu@icloud.com>
2025-07-26 14:26:21 +0300

    değişkenler

---
+++ script.js
@@ -0,0 +1,13 @@
+var urunAdi = "ıphone 13"; //string
+let urunFiyat = "15000"; // number
+console.log(typeof urunAdi);
+console.log(typeof urunFiyat);
+let sayi1 = 10;
+let sayi2 = 12;
+console.log(sayi1 + sayi2);
+let isim = "helin";
+let soyad = " kurt";
+console.log(isim + soyad);
+let sinavNotu = 70;
+let basarilimi = (sinavNotu >= 50);
+console.log(basarilimi);
