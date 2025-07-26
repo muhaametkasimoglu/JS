66090b00a34f462cbeef91f31f7d95157d34991e
muhaametkasimoglu <muhammetkasimoglu@icloud.com>
2025-07-26 14:43:39 +0300

    quiz

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-var urunAdi = "ıphone 13"; //string
-let urunFiyat = "15000"; // number
-console.log(typeof urunAdi);
-console.log(typeof urunFiyat);
-let sayi1 = 10;
-let sayi2 = 12;
-console.log(sayi1 + sayi2);
-let isim = "helin";
-let soyad = " kurt";
-console.log(isim + soyad);
-let sinavNotu = 70;
-let basarilimi = (sinavNotu >= 50);
-console.log(basarilimi);
+let ogrenci1Ad = " muhammet";
+let ogrenci1Soyad = " kasımoğlu";
+
+let ogrenci1dogumTarihi = "2002";
+let ogrenci1matematik1 = 70;
+let ogrenci1matematik2 = 60;
+let ogrenci1matematik3 = 80;
+let ogrenci2Ad = " helin ";
+let ogrenci2Soyad = " kurt";
+let ogrenci2dogumTarihi = "2003";
+let ogrenci2matematik1 = 70;
+let ogrenci2matematik2 = 60;
+let ogrenci2matematik3 = 80;
+let ogrenci1yas = 2025 - parseIntogrenci1dogumTarihi;
+let ogrenci2yas = 2025 - parseIntogrenci2dogumTarihi;
+console.log(ogrenci1yas);
+console.log(ogrenci2yas);
