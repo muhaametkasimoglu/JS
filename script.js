acd1653a3deffd61774aa27a73cca44f89ba1fa7
muhaametkasimoglu <muhammetkasimoglu@icloud.com>
2025-07-29 12:12:29 +0300

    for döngüsü

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-let urun1 = "ıphone 12";
-let urun2 = " ıphone13";
-let urun3 = " ıphone 14";
-
-let urunler = ["ıphone 12", "ıphone 13", "ıphone 14"];
-let fiyatlar = [9000, 12000, 20000];
-let renkler = ["gold", "beyaz", "siyah"];
-console.log(urunler[0]);
-console.log(urunler[1]);
-console.log(urunler[2]);
-console.log(renkler);
-let kursAdi = "komple uygulamalı web geliştime";
-console.log(kursAdi[5]);
+let toplam = 0;
+for (let i = 1; i <= 0; i++) {
+  toplam += i;
+}
+console.log(toplam);
+let sayilar = [1, 24, 5, 8, 4];
+for (let i = 0; i < 6; i++) {
+  console.log(sayilar[i]);
+}
